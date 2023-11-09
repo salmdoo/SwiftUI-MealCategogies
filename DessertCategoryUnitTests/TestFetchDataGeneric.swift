d72386f59d0fd41b6c07174c3061d8ceaa1e615b
Salm Do <thaodo.usa@gmail.com>
2023-11-09 15:20:37 -0600

    clean code

--- DessertCategoryUnitTests/TestFetchDataGeneric.swift
+++ DessertCategoryUnitTests/TestFetchDataGeneric.swift
@@ -97,46 +97,5 @@ final class TestFetchDataGeneric: XCTestCase {
         expectation.fulfill()
         await self.fulfillment(of: [expectation], timeout: 10)
     }
-    
-//    func testFetchDataGeneric_DecodedFailed_ReturnFailure() async {
-//
-//        let expectedResultStr = "[{\"_id\":\"123\"}]"
-//        MockFetchDataGeneric.responseData = expectedResultStr.data(using: .utf8)
-//
-//        let expectation = self.expectation(description: "Fetch Data is failed because of decodedFailed")
-//        let fetchDataGeneric = FetchDataGeneric<TestObjectFailed>(urlSession: urlSession!, urlApi: "https://example.com/api")
-//        let result = await fetchDataGeneric.fetchData()
-//        switch result {
-//        case .failure(let err):
-//            XCTAssertEqual(NetworkError.decodedFailed, err)
-//        default:
-//            XCTFail("Expected decodedFailed error but it does not")
-//        }
-//        expectation.fulfill()
-//        await self.fulfillment(of: [expectation], timeout: 10)
-//    }
-    
-//    func testFetchDataGeneric_ValidData_ReturnSuccess() {
-//        let expectedResultStr = "[{\"name\":\"Check\"}]"
-//        MockFetchDataGeneric.responseData = expectedResultStr.data(using: .utf8)
-//
-//        let expectation = self.expectation(description: "Fetch Data is success")
-//
-//        let fetchDataGeneric = FetchDataGeneric<TestObject>(urlSession: urlSession!, urlApi: "https://example.com/api")
-//        
-//        Task {
-//                let result = await fetchDataGeneric.fetchData()
-//
-//                switch result {
-//                case .success(let res):
-//                    XCTAssertEqual(res, TestObject(name: "Check"))
-//                default:
-//                    XCTFail("Expected success but it does not")
-//                }
-//                expectation.fulfill()
-//
-//        }
-//
-//        self.wait(for: [expectation], timeout: 10)
-//        }
+
 }
